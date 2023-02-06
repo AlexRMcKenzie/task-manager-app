a2e66bc96fbe9819ab5530a753e624e78fe0dd2d
Alex McKenzie <alexmckenzie1990@gmail.com>
2023-02-06 14:40:30 +0000

    Added functionality to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import './App.css';
+// Components
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+// Hooks
+import { useState, useEffect } from 'react';
+// Packages
+import { v4 as uuidv4 } from 'uuid';
+import Swal from "sweetalert2";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [showAddTask, setShowAddTask] = useState(false);
   return (
-    <h1>Hello World</h1>
-  );
+    <>
+      <div className="container">
+        {/*App Header */}
+        <Header showForm={() => setShowAddTask(!showAddTask)} changeTextAndColor={showAddTask} />
+        {/* Revealing the Add Task Form */}
+        {showAddTask && <AddTask onSave={addTask} />}
+
+        {/* Displaying Tasks */}
+        {
+          tasks.length > 0 ?
+          (<Tasks tasks={tasks} />) :
+          ('No Task Found!')
+        }
+      </div>
+    </>
+  )
 }
+
 export default App;
